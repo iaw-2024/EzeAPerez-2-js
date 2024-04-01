fcbb919794bd23a2a588ad48a97bcb1daaffa2b3
EzePerez <ezequielalejandroperez@hotmail.com>
2024-04-01 12:29:34 -0300

    modificacion en el dom.js de cliente servidor

--- public/cliente_servidor/dom.js
+++ public/cliente_servidor/dom.js
@@ -47,7 +47,7 @@ function crearListaJugadores(posicion) {
   lista.classList.add("list-group");
   lista.classList.add("list-group-flush");
   imagen = document.createElement("img");
-  imagen.src = `${/public/ + jsonResponse.SeleccionArgentina[posicion].imagen}`;
+  imagen.src = `${jsonResponse.SeleccionArgentina[posicion].imagen}`;
   imagen.style.width = "35px";
   for (const jugadores in jsonResponse.SeleccionArgentina[posicion].jugadores) {
     li = document.createElement("li");
